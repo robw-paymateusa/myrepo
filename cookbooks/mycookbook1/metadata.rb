006e6fc7d3efb45a84039196a5ff9868dde138ab
robw-paymateusa <robw@paymateusa.com>
2013-01-21 23:14:29 +0000

    Added rightscale_marker to default.rb

--- cookbooks/mycookbook1/metadata.rb
+++ cookbooks/mycookbook1/metadata.rb
@@ -5,4 +5,6 @@ description      "Installs/Configures mycookbook1"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
+depends "rightscale"
+
 recipe "mycookbook1::default","Prints hello world output"
