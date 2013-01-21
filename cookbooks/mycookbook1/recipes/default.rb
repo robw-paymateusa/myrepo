7e2aea6264f09cffcfc31ff74d8879671026dc6c
robw-paymateusa <robw@paymateusa.com>
2013-01-21 22:05:32 +0000

    Added a hello world message to the default.rb recipe

---
+++ cookbooks/mycookbook1/recipes/default.rb
@@ -0,0 +1,10 @@
+#
+# Cookbook Name:: mycookbook1
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+log "Hello World! You should try Chef with RightScale!"
