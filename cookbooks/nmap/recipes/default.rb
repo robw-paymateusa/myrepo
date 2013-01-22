0853594cab11e84e0da188d3ecbda9f7667d5215
robw-paymateusa <robw@paymateusa.com>
2013-01-22 02:08:51 +0000

    initial commit for nmap

---
+++ cookbooks/nmap/recipes/default.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: nmap
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+package "nmap" do
+    action [:install]
+    end
